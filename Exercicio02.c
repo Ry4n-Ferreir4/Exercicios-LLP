3751c24a56e99241eceaf4b90816f5f3cabdfc2a
Ry4n-Ferreir4 <130564393+Ry4n-Ferreir4@users.noreply.github.com>
2023-04-12 15:07:54 -0300

    Update Exercicio02.c

--- Exercicio02.c
+++ Exercicio02.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
-int main(void) {
-  int matrix[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
-  int matrix2[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
-  int matrizSoma[3][3];
-  
+int main() {
+  int matrix1[3][3] = { {6, 5, 12}, {1, 6, 85}, {15, 25, 30} };
+  int matrix2[3][3] = { {1, 4, 2}, {3, 6, 8}, {1, 7, 4} };
+  int matrixSoma[3][3];
+
   int i, j;
-
-  for (i = 0; i < 3; i++){
-    for (j = 0; j < 3; j++){
-     
-        matrizSoma[i][j] = matrix[i][j] + matrix2[i][j];
-        printf("Matriz[%d][%d]: %d\n", i, j, matrizSoma[i][j]);
-      
-      }
+  printf("Matriz =\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf(" %d", matrix1[i][j]);
+    }
+    printf("\n");
+    
   }
-
+  
+  printf("\n\n Matriz =\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf(" %d", matrix2[i][j]);
+    }
+    printf("\n");
+    
+  }
+  
+  printf("\n\n MatrizSoma =\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+    matrixSoma[i][j] = matrix1[i][j] + matrix2[i][j];
+      printf(" %d", matrixSoma[i][j]);
+    }
+    printf("\n");
+    }
   return 0;
 }
