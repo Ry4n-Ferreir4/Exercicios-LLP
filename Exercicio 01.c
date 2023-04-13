25f334f2e39b4d0586fe49190b2d60627e5e086e
Ry4n-Ferreir4 <ryan.mendes07@aluno.ifce.edu.br>
2023-04-13 00:08:31 -0300

    Primeiro Commit

---
+++ Exercicio 01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+
+  int i, j;
+ 
+  for (i = 0; i < 3; i++){
+    for (j = 0; j < 3; j++){
+      
+      if (i == j):
+        printf("Matriz[%d][%d]: %d\n", i, j, matrix[i][j]);
+      
+      }
+  }
+
+  return 0;
+}
---
+++ Exercicio02.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+  int matrix1[3][3] = { {6, 5, 12}, {1, 6, 85}, {15, 25, 30} };
+  int matrix2[3][3] = { {1, 4, 2}, {3, 6, 8}, {1, 7, 4} };
+  int matrixSoma[3][3];
+
+  int i, j;
+  printf("Matriz =\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf(" %d", matrix1[i][j]);
+    }
+    printf("\n");
+    
+  }
+  
+  printf("\n\n Matriz2 =\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf(" %d", matrix2[i][j]);
+    }
+    printf("\n");
+    
+  }
+  
+  printf("\n\n MatrizSoma =\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+    matrixSoma[i][j] = matrix1[i][j] + matrix2[i][j];
+      printf(" %d", matrixSoma[i][j]);
+    }
+    printf("\n");
+    }
+  return 0;
+}
